fd2d5826ec2b0a70d60b345a3773683dcc50e4f8
RolandCrow <yevgeniy_yeltsov@mail.ru>
2024-11-29 18:00:23 +0300

    Сделал WindowSizeClassDemoActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,8 @@ dependencies {
     implementation(libs.androidx.runtime.livedata)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.window)
+    implementation(libs.androidx.compose.material3.windowSizeClass)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
